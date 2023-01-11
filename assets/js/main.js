af56d9e9e9dbe0807f9d0587669183af44940bd9
Tiago Medeiros <tiago.medeiros11@gmail.com>
2023-01-11 17:52:56 -0300

    iniciado o consumo da api pra atualizar o HTML dinamicamente (Realizado no trabalho)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,34 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then((response) => {
-  console.log(response);
-});
+function converterPokemonParaHtml(pokemon) {
+  return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+
+            </li>
+    `;
+}
+
+console.log(document.getElementById("listaDePokemons"));
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((jsonBody) => jsonBody.results)
+//   .then((pokemonList) => {
+//     for (let i = 0; i < pokemonList.length; i++) {
+//       const pokemon = pokemonList[i];
+//       console.log(converterPokemonParaHtml(pokemon));
+//     }
+//   })
+//   .catch((error) => console.error(error));
