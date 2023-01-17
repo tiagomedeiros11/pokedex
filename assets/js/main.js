8e90fe078da0d9abe4de4b0aab112e4112d6971a
Tiago Medeiros <tiago.medeiros11@gmail.com>
2023-01-17 15:39:35 -0300

    Adicionado um novo arquivo javaSript, onde foi criado uma classe que serpara o que realmente queremos da pokeapi como ajustei todo o codigo para esse novo formato com as classes, e tambem adicionado uma classe dinamica para alterar a cor do card dinamicamente baseado no tipo do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,18 @@
 
 
 
-function converterTiposDePokemonsParaLi(pokemonType) {
-  return pokemonType.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 // função para converter o pokemon em um item da lista da pokedex em html
 function converterPokemonParaListaNoHTML(pokemon) {
   return `
-    <li class="pokemon">
-                <span class="number">#0${pokemon.id}</span>
+    <li class="pokemon ${pokemon.type} ">
+                <span class="number">#0${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${converterTiposDePokemonsParaLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    <img src="${pokemon.image}"
                         alt="${pokemon.name}">
                 </div>
 
@@ -27,7 +23,6 @@ function converterPokemonParaListaNoHTML(pokemon) {
 const $pokemonsNaOL = document.getElementById("listaDePokemons");
 
 //função que itera a lista do retorno da função get.Pokemons e adiciona no html
-
 pokeApi.getPokemons().then((pokemonList = []) => {
   $pokemonsNaOL.innerHTML += pokemonList.map(converterPokemonParaListaNoHTML).join('')
 });
